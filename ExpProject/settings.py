0bf1f2e2384ffcf11543168fb0ee25b602f4aad7
VK <vedran.kusic@gmail.com>
2022-03-21 14:54:08 +0100

    Added files and changes required for deployment to heroku

--- ExpProject/settings.py
+++ ExpProject/settings.py
@@ -137,5 +137,6 @@ DATABASES['default'].update(db_from_env)
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
